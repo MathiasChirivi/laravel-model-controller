df2249e3c6b0f16006b66472ebddeb7e3e96eb5c
MathiasChirivi <chirivimathias3@gmail.com>
2023-06-28 17:07:20 +0200

    ese finito

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,5 @@
 <header class="p-3">
-
+    <div class="d-flex justify-content-center container">
+        <h3>List Movies</h3>
+    </div>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container my-3">
-    <h1>Welcome Page</h1>
-    <div class="row g-4">
-        <div class="col">
-            <div>
-                @foreach ($movies as $movie)
-                    <h2 class=""> {{ $movie->title }} </h2>
-                @endforeach
+    <div class="mt-5 mb-5 container text-center">
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+            @foreach ($movies as $movie)
+            <div class="col">
+                <div class="card p-3" style="width: 15rem; height: 18rem">
+                    <div class="card-body">
+                        <h4 class=""> {{ $movie->original_title }} </h4>
+                        <p class="d-flex flex-column flex-nowrap"><strong>Nazionality movies:</strong> {{ $movie->nationality }} </p>
+                        <p class="d-flex flex-column flex-nowrap"><strong>Date publish:</strong> {{ $movie->date }} </p>
+                        <p class="d-flex flex-column flex-nowrap"><strong>Vote:</strong> {{ $movie->vote }} </p>
+                    </div>
+                </div>
             </div>
+            @endforeach
         </div>
     </div>
-
-</div>
 @endsection
